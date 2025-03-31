3ebde6a29d38ac54975579b64bf05023e735072a
tuchan2002 <trananhtu171002@gmail.com>
2025-03-31 14:30:02 +0700

    test model

--- voc_dataset.py
+++ voc_dataset.py
@@ -31,7 +31,7 @@ class VOCDataset(VOCDetection):
 
 if __name__ == '__main__':
   transform = ToTensor()
-  dataset = VOCDataset(root="my_pascal_voc", year="2012", image_set="train", download=False, transform=transform)
+  dataset = VOCDataset(root="my_pascal_voc", year="2012", image_set="train", download=True, transform=transform)
   image, target = dataset[2000]
   print(image.shape)
   print(target)
